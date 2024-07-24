cbe2185962a4cdd7f7744e615893ecb50a96f8db
justnotiyan <muhammadfitrian0712@gmail.com>
2024-07-24 16:32:14 +0700

    adding marktext and collect function on database seeder

--- README.md
+++ README.md
@@ -3,3 +3,38 @@
 ```bash
 npm install && composer install
 ```
+
+or with bun
+
+```bash
+bun install && composer install
+```
+
+**Renaming .env.example to .env**
+
+```bash
+mv .env.example .env
+```
+
+**Set your database**
+
+```bash
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=laravel
+DB_USERNAME=root
+DB_PASSWORD=
+```
+
+**Then Generate Key**
+
+```bash
+php artisan key:generate --ansi
+```
+
+**Running all migrations and seeders**
+
+```bash
+php artisan migrate:fresh --seed
+```
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,16 +7,27 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+    \App\Models\User::factory()->create([
+      'name' => 'admin',
+      'email' => 'test@example.com',
+      'password' => bcrypt('root')
+    ]);
+
+    // collect([
+    //   ['name' => 'Software Engineer'],
+    //   ['name' => 'Hr Manager'],
+    //   ['name' => 'Finance Analyst'],
+    //   ['name' => 'Marketing Manager'],
+    //   ['name' => 'Operations Manager'],
+    // ])->each(fn ($data) => \App\Models\Position::factory()->create($data));
+
+
+  }
 }
